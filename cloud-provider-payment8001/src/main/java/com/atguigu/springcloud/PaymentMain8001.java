c3a1e13bb8a16c3a3fdef34492e264fb52954ffd
FengYuZeXi <1075357436@qq.com>
2022-08-22 23:55:12 +0800

    PaymentMain8001 module 创建/pom文件的修改/yml文件的创建

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+/**
+ * @Description: 启动类
+ * @author:fengchengwen
+ * @createDate: 2022-08-22 23:29
+ */
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+
+    }
+}
