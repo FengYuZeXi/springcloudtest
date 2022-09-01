eaee28517c43f6cc5997ce1c306fd8d58f55ed0f
fcw <1075357436@qq.com>
2022-09-01 14:11:31 +0800

    修改pom文件以及配置启动器

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -1,13 +1,18 @@
 package com.atguigu.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description: 启动类
  * @author:fengchengwen
  * @createDate: 2022-08-22 23:29
  */
+
+@SpringBootApplication
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(PaymentMain8001.class,args);
     }
 }
